ae88ff8764c817e3530554fc39fa60ad3a8c1f26
Anjali Joshi <joshia21@mcmaster.ca>
2025-04-15 18:26:35 -0400

    Updated database connection and SQL files

--- part 1/db.php
+++ part 1/db.php
@@ -1,7 +1,7 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$password = 'root';
+$host = 'tmamysqlserver.mysql.database.azure.com';
+$user = 'server_admin@tmamysqlserver';
+$password = 'login_TMA2';
 $database = 'bookmark_db';
 
 $conn = new mysqli($host, $user, $password, $database);
--- part 2/db.php
+++ part 2/db.php
@@ -1,7 +1,7 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$password = 'root';
+$host = 'tmamysqlserver.mysql.database.azure.com';
+$user = 'server_admin@tmamysqlserver';
+$password = 'login_TMA2';
 $database = 'lms_db';
 
 $conn = new mysqli($host, $user, $password, $database);
