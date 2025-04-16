ce1224e5414306f4ed04d7c85b27cb40c0bcee8b
-learning-app <joshia21@mcmaster.ca>
2025-04-16 00:26:36 -0400

    fixing folder names

---
+++ part1/db.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'tmamysqlserver.mysql.database.azure.com';
+$user = 'server_admin';
+$password = 'login_TMA2';
+$database = 'bookmark_db';
+$ssl_cert = __DIR__ . '/../DigiCertGlobalRootG2.crt.pem';
+
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, $ssl_cert, NULL, NULL);
+
+if (!mysqli_real_connect($conn, $host, $user, $password, $database, 3306, NULL, MYSQLI_CLIENT_SSL)) {
+    die('Connection failed: ' . mysqli_connect_error());
+}
+?>
