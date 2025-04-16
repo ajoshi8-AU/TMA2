95344a857aa6fa5d79143dfbe40e940dd4a06772
ajoshi8-AU <ajoshi8@learn.athabascau.ca>
2025-04-16 18:29:31 -0400

    Update db.php

--- part2/db.php
+++ part2/db.php
@@ -3,12 +3,12 @@ $host = 'tmamysqlserver.mysql.database.azure.com';
 $user = 'server_admin';
 $password = 'login_TMA2';
 $database = 'lms_db';
-$ssl_cert ='/../DigiCertGlobalRootG2.crt.pem';
+$ssl_cert = '../DigiCertGlobalRootG2.crt.pem';
 
-$conn = new mysqli($host, $user, $password, $database);
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, $ssl_cert, NULL, NULL);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!mysqli_real_connect($conn, $host, $user, $password, $database, 3306, NULL, MYSQLI_CLIENT_SSL)) {
+    die('Connection failed: ' . mysqli_connect_error());
 }
-?>
 ?>
