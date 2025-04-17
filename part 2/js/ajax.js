af8274412d4d3d34bc3575753e9b22cc496e571a
ajoshi8-AU <ajoshi8@learn.athabascau.ca>
2025-04-16 22:40:41 -0400

    "Removing the outdated version"

--- part 2/js/ajax.js
+++
@@ -1,7 +0,0 @@
-function loadLesson(id) {
-    fetch("lesson.php?id=" + id)
-        .then(res => res.text())
-        .then(data => {
-            document.getElementById("lesson").innerHTML = data;
-        });
-}
